7e945993e7fcbc8dcbb8d09fdddc5816eed37bf6
YassineRazkaoui <razkaouiyassine9@gmail.com>
2024-04-16 13:16:54 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "../src/components/Coaching/Navbar";
 import Box1 from "./components/Coaching/Box1";
 import Box2 from "./components/Coaching/Box2";
 import Box3 from "./components/Coaching/Box3";
+import Box4 from "./components/Coaching/Box4";
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Box1/>
       <Box2/>
       <Box3/>
+      <Box4/>
     </div>
   )
 };
---
+++ src/components/Coaching/Box4.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../../styles/Coaching/Box4.css";
+
+function Box4() {
+  return (
+    <div className="box4-cont">
+      <div className="box4-el">
+        <div className="bac-box4">
+          <div className="info-box4">
+            <h1 className="h1-box4">HOW TO SOLVE CHILDREN'S PROBLEMS</h1>
+            <p className="p-box4">
+              Le Lorem Ipsum est simplement du faux texte employé dans la
+              composition et la mise en page avant impression. Le Lorem Ipsum
+              est le faux texte standard de l'imprimerie depuis les années 1500,
+              quand un imprimeur anonyme assembla ensemble des morceaux de texte
+              pour réaliser un livre spécimen de polices de texte. Il n'a pas
+              fait que survivre cinq siècles, mais s'est aussi adapté à la
+              bureautique informatique, sans que son contenu n'en soit modifié.
+              Il a été popularisé dans les années 1960 grâce à la vente de
+              feuilles Letraset contenant des passages du Lorem Ipsum, et, plus
+              récemment, par son inclusion dans des applications de mise en page
+              de texte, comme Aldus PageMaker.
+            </p>
+            <div className="btn-box4">
+              <button className="tex-box4">
+                <span className="tt-box4">SEE METHOD’S</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Box4;
