b0bb2faea415b85c0743c08c29c30cf3234cade6
BRAHIM OUDRA <126688886+Oudra1729@users.noreply.github.com>
2024-04-24 15:45:37 +0100

    Update PageInformatique.jsx

--- src/components/Informatique/PageInformatique.jsx
+++ src/components/Informatique/PageInformatique.jsx
@@ -1,1 +1,26 @@
 
+import React from "react";
+import Navbar from "./components/Navbar";
+import Info1 from ".Info1";
+import Info2 from "./Info2";
+import Info3 from "./Info3";
+import Info4 from ".Info4";
+import Info5 from ".Info5";
+import Footer from "./components/Footer";
+
+
+const PageInformatique = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Info1/>
+      <Info2/>
+      <Info3/>
+      <Info4/>
+      <Info5/>
+      <Footer/>
+    </div>
+  )
+};
+
+export default PageInformatique;
